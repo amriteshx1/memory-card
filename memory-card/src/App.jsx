1d575d62832d8382d03b814ae90a0634ac9917c4
amriteshx1 <amriteshx18@gmail.com>
2025-03-21 08:42:31 +0530

    Add initial components structure for the application

---
+++ memory-card/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function Header(){
+  return (
+    <div className="cnt1">
+
+    </div>
+  )
+}
+
+function Main(){
+  
+  return (
+    <div className="cnt2">
+      
+    </div>
+  )
+}
+
+
+
+function App(){
+  return(
+    <div className="container">
+      <Header />
+      <Main />
+    </div>
+  )
+}
+
+export default App;
